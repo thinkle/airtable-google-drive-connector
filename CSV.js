d5fa3118125ba94b9e24c78db61055cfd4a313b3
Tom Hinkle <tmhinkle@gmail.com>
2022-08-16 07:47:37 -0400

    Spacing (lint)

--- CSV.js
+++ CSV.js
@@ -1,13 +1,13 @@
-function readCsvToJson (fileId) {
+function readCsvToJson(fileId) {
   let file = DriveApp.getFileById(fileId);
   let blob = file.getBlob().getDataAsString();
-  let csv = Utilities.parseCsv(blob)
+  let csv = Utilities.parseCsv(blob);
   let jsonRows = [];
   let headers = csv[0];
-  for (let rn=1; rn<csv.length; rn++) {
+  for (let rn = 1; rn < csv.length; rn++) {
     let row = csv[rn];
-    let json = {}
-    for (let cn=0; cn<headers.length; cn++) {
+    let json = {};
+    for (let cn = 0; cn < headers.length; cn++) {
       json[headers[cn]] = row[cn];
     }
     jsonRows.push(json);
